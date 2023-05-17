ea1abdaf5a1663c284ec75bc0f0100aa3e8eea09
Eric C <39015447+ricetech@users.noreply.github.com>
2023-05-16 21:29:51 -0400

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+        "@remix-run/eslint-config",
+        "@remix-run/eslint-config/node",
+        "prettier",
+    ],
+    rules: {
+        "prefer-template": "warn",
+        "require-await": "error",
+    },
+};
